c13c86bb52cc4a21de87aadf963a984ba8b9750e
trueStoryJapan <juanca_mt@hotmail.com>
2021-02-18 02:07:42 +0900

    feat: add http service layer

---
+++ src/services/pollService.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { axiosInstance } from 'services/apiCore';
+import { PollResponse } from 'types/api';
+
+export const poll = {
+  fetch: (): Promise<PollResponse> => {
+    return axiosInstance.get<PollResponse>('/poll').then((res) => {
+      return res.data;
+    });
+  },
+};
