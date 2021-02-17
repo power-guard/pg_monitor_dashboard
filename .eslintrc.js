0f9b967c70bcfe3cfa2c5d6e6d6b979052a92421
Juan Carlos M. Tallon <42070238+jcmtallon@users.noreply.github.com>
2021-02-17 23:34:42 +0900

    build: create nextjs app (#12)
    * build: init create-next-app
    
    * build: install dependencies
    
    * build: set up typescript
    
    * build: add nvmrc file
    
    * build: add npmrc file
    
    * build: set up eslint + prettier
    
    * style: add missing end lines

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
