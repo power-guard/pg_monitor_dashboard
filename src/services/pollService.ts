a6e6d9e12b9e9bcc6820c40a1e52adfac6fb3213
trueStoryJapan <juanca_mt@hotmail.com>
2021-02-20 10:53:47 +0900

    fix: Move env variable to .env.local

--- src/services/pollService.ts
+++ src/services/pollService.ts
@@ -4,7 +4,7 @@ import { PollResponse } from 'types/api';
 
 export const poll = {
   fetch: (): Promise<PollResponse> => {
-    if (process.env.NEXT_PUBLIC_FETCH_MOCK_DATA) return new Promise((resolve) => resolve(mockPollResponse));
+    if (process.env.NEXT_PUBLIC_FETCH_MOCK_DATA === 'true') return new Promise((resolve) => resolve(mockPollResponse));
 
     return axiosInstance.get<PollResponse>('/poll').then((res) => {
       return res.data;
