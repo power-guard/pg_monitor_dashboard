da60c6e05d87994bfdc057d9d21e601b6bb1a907
Power Guard <66584472+power-guard@users.noreply.github.com>
2021-02-22 10:17:24 +0900

    Feature/issue17 test server (#18)
    * feat: Poll response includes ordered array of MonitoringData
    
    * feat: Add example with error status
    
    Co-authored-by: Dan Liberatori <dan.liberatori@power-guard.jp>

--- srv/README.md
+++ srv/README.md
@@ -1,6 +1,19 @@
 # Demo server
 
-## Getting started
+## Getting started (Docker Compose)
+
+1. From inside the `/srv` directory, run Docker Compose as follows:
+
+```shell
+docker-compose up
+```
+
+2. Access the web server at `http://localhost:23500/api/dashboard/poll`
+
+3. Stop the container with CTRL+C.
+
+
+## Getting started (Docker)
 
 1. From inside the `/srv` directory, build the docker image.
 
@@ -14,9 +27,13 @@ docker build -t pg_monitor_demo_srv .
 docker run -p 23500:23500 -v $(pwd):/code pg_monitor_demo_srv
 ```
 
-3. Access the web server at `http://localhost:23500/`
+3. Access the web server at `http://localhost:23500/api/dashboard/poll`
+
+4. Stop the container with CTRL+C.
+
 
 ## Modifying this test server
 
-The main code for responding to requests lives in `pg_monitor_demo_srv/pg_monitor_demo_srv/views.py`.
-The main code for generating a response lives in `pg_monitor_demo_srv/pg_monitor_demo_srv/worker.py`.
+The main code for responding to requests lives in `pg_monitor_demo_srv/dashboard/views.py`.
+The main code for generating a response lives in `pg_monitor_demo_srv/dashboard/worker.py`.
+Response models are specified in `pg_monitor_demo_srv/dashboard/models.py`.
---
+++ srv/pg_monitor_demo_srv/dashboard/views.py
@@ -0,0 +1,80 @@
+import logging
+import datetime
+import threading
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import PollResponse
+from .worker import dummy_worker_sma, dummy_worker_solar_edge, dummy_worker_ng
+
+logger = logging.getLogger('pg-dashboard')
+
+plant_tuples = [
+    ('PGOM100001', 'PV System 1'),
+    ('PGOM100002', 'PV System 2'),
+    ('PGOM100003', 'ミラクル福岡パワーフルな太陽光発電所３'),
+    ('PGOM100004', 'PGOM100004'),
+    ('PGOM100005', '鹿児島県日置市吹上町'),
+    ('PGOM100006', 'Faulty Power Plant'),
+]
+response_data = None
+
+workers = dict()
+
+# Create your views here.
+@csrf_exempt
+def poll(request):
+    global response_data
+
+    if not response_data:
+        # first time the endpoint is hit after server boot
+        logger.info('Launching workers for the first time.')
+        response_data = PollResponse(plant_tuples)
+        launch_workers()
+
+    elif not response_data.proc_end:
+        logger.info(f'Still processing. Request initiated at {response_data.proc_start}')
+        # Workers are fetching data.
+        # Return the response as it is right now.
+        # Client will poll again in a few seconds
+        pass
+
+    else:
+        # A response was completed at some point in the past.
+        # If the response is stale, we need to start a new one.
+        diff = datetime.datetime.now() - response_data.proc_end
+        cutoff = datetime.timedelta(minutes=5)
+        is_stale = diff > cutoff
+        if is_stale:
+            logger.info('Response is stale. Launching workers.')
+            response_data = PollResponse(plant_tuples)
+            launch_workers()
+        else:
+            logger.info(f'Response is less than {str(cutoff)} old. Returning it as-is.')
+
+    return JsonResponse(response_data.serialize())
+
+
+def launch_workers():
+    global workers
+    global response_data
+    try:
+        workers['sma'] = threading.Thread(target=dummy_worker_sma, args=(response_data,))
+        workers['sma'].start()
+    except:
+        logger.exception('Failed to start SMA thread')
+        raise
+
+    try:
+        workers['se'] = threading.Thread(target=dummy_worker_solar_edge, args=(response_data,))
+        workers['se'].start()
+    except:
+        logger.exception('Failed to start SE thread')
+        raise
+
+    try:
+        workers['ng'] = threading.Thread(target=dummy_worker_ng, args=(response_data,))
+        workers['ng'].start()
+    except:
+        logger.exception('Failed to start SE thread')
+        raise
