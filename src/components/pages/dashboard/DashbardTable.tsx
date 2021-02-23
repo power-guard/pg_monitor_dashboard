da0f314c8a0a5aaea132816249201b989a00491d
trueStoryJapan <juanca_mt@hotmail.com>
2021-02-23 13:21:20 +0900

    feat: add modal to page and handle state

--- src/components/pages/dashboard/DashbardTable.tsx
+++ src/components/pages/dashboard/DashbardTable.tsx
@@ -1,41 +1,60 @@
-import React from 'react';
-import { Plants } from 'types/api';
+import React, { useState } from 'react';
+import { MonitorData, Plants } from 'types/api';
+
+import { DashboardPlantModal } from './DashboardPlantModal';
 
 interface OwnProps {
   plants: Plants;
 }
 
 export const DashboardTable = ({ plants }: OwnProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activePlant, setActivePlant] = useState<MonitorData>();
+
+  const onSystemIdClick = (selectedPlant: MonitorData) => {
+    setActivePlant(selectedPlant);
+    setIsModalOpen((isModalOpen) => !isModalOpen);
+  };
+
+  const onModalClose = () => {
+    setActivePlant(undefined);
+    setIsModalOpen(false);
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>System ID/Name</th>
-          <th>Last Update Time</th>
-          <th>Logs</th>
-          <th>Energy Series</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.entries(plants).map(([id, data]) => (
-          <tr key={id}>
-            <td>
-              {id} {data.name}
-            </td>
-            <td>{data.last_update}</td>
-            <td>
-              {data.logs.map((log, index) => (
-                <div key={index}>
-                  {log.level}
-                  {log.date}
-                  {log.description}
-                </div>
-              ))}
-            </td>
-            <td>Table</td>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>System ID/Name</th>
+            <th>Last Update Time</th>
+            <th>Logs</th>
+            <th>Energy Series</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {Object.entries(plants).map(([id, data]) => (
+            <tr key={id}>
+              <td>
+                <div onClick={() => onSystemIdClick(data)}>{id}</div>
+                {data.name}
+              </td>
+              <td>{data.last_update}</td>
+              <td>
+                {data.logs.map((log, index) => (
+                  <div key={index}>
+                    {log.level}
+                    {log.date}
+                    {log.description}
+                  </div>
+                ))}
+              </td>
+              <td>Table</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {isModalOpen && <DashboardPlantModal data={activePlant} onClose={onModalClose} />}
+    </>
   );
 };
