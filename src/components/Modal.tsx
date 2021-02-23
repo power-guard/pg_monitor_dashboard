11a6d0b285c3f3d59ca776461bddf8409ba68fd6
trueStoryJapan <juanca_mt@hotmail.com>
2021-02-23 13:20:21 +0900

    feat: Add modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,22 @@
+import { useClickOutsideListenerRef } from 'hooks/useClickOutsideListenerRef';
+import React, { ReactNode } from 'react';
+
+interface OwnProps {
+  onClose: () => void;
+  children?: ReactNode;
+}
+
+export const Modal = ({ children, onClose }: OwnProps) => {
+  const ref = useClickOutsideListenerRef(onClose);
+
+  return (
+    <>
+      <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div ref={ref} className="relative w-auto mx-auto my-6 max-w-3xl">
+          {children}
+        </div>
+      </div>
+      <div className="opacity-40 fixed inset-0 z-40 bg-black"></div>
+    </>
+  );
+};
