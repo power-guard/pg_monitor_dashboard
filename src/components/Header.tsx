16e877d5b685b7237075a408f9fdd4481ac79fff
trueStoryJapan <juanca_mt@hotmail.com>
2021-02-26 01:56:07 +0900

    feat: style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export const Header = () => (
-  <header>
-    <div>
-      <div>Power Guard Monitor</div>
+  <header className="bg-gradient-to-r from-brand-400 to-brand-600 w-full fixed shadow-md z-10">
+    <div className="min-w-screen-xl max-w-screen-2xl m-auto px-4 py-2">
+      <div className="text-white text-2xl select-none">Power Guard Monitor</div>
     </div>
   </header>
 );
