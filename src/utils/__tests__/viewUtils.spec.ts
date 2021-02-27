14835107b568e21e82eb50ac7d08d7c19f9eaa94
trueStoryJapan <juanca_mt@hotmail.com>
2021-02-27 10:41:45 +0900

    feat: Add validation to data parsing utils

--- src/utils/__tests__/viewUtils.spec.ts
+++ src/utils/__tests__/viewUtils.spec.ts
@@ -1,4 +1,4 @@
-import { getFormattedDate } from 'utils/viewUtils';
+import { getDistanteToNow, getFormattedDate } from 'utils/viewUtils';
 
 describe('getIsoToLocalDate', () => {
   it('transforms UTC iso to local time date', () => {
@@ -9,5 +9,15 @@ describe('getIsoToLocalDate', () => {
   it('parses time without T and miliseconds', () => {
     const date = getFormattedDate(`2021-02-20 00:15:00`);
     expect(date).toBe('2021-02-20 09:15:00');
+  });
+
+  it('Returns "Invalid date" string when invalid date format received', () => {
+    const date = getFormattedDate(`20-02-2021 00:15:00`);
+    expect(date).toBe('Invalid date');
+  });
+
+  it('Returns "Invalid time" string when invalid date format received', () => {
+    const date = getDistanteToNow(`20-02-2021 00:15:00`);
+    expect(date).toBe('Invalid time');
   });
 });
