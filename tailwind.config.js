7f79d6711146f9a5237604fd36725080be05abb9
trueStoryJapan <juanca_mt@hotmail.com>
2021-03-01 23:07:42 +0900

    fix: typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
